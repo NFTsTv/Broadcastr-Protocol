63c1700ca80318c52b3b25a3c543406853ffdeca
Mario S.O <gh_mario.proclaim@slmail.me>
2023-03-15 10:42:19 -0400

    Upgradeable contracts on the works

---
+++ src/upgradeables/CastrFactory.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/proxy/beacon/BeaconProxy.sol";
+import "./CastrBeacon.sol";
+import "./Castr.sol";
+
+contract CastrFactory {
+    CastrBeacon immutable beacon;
+
+    constructor(address _blueprint) {
+        beacon = new CastrBeacon(_blueprint);
+    }
+
+    function createCastr(
+        string calldata _baseTokenURI,
+        string calldata _name,
+        string calldata _description,
+        bool _limitedSupply,
+        uint256 _totalSupply,
+        uint256 _mintPrice
+    )
+        public
+        returns (address)
+    {
+        BeaconProxy proxy = new BeaconProxy(
+            address(beacon), 
+            abi.encodeWithSelector(Castr(address(0)).initialize.selector, _baseTokenURI, _name, _description, _limitedSupply, _totalSupply, _mintPrice));
+        return address(proxy);
+    }
+
+    function getBeacon() public view returns (address) {
+        return address(beacon);
+    }
+}
